df84354482393bcd355078021eeee48e65508da4
Rashid Carlos Jamil Marcano-Rivera <67828343+RashidCJ@users.noreply.github.com>
2025-10-07 15:10:16 -0400

    Add files via upload

---
+++ Tareas de SOCI 1ᴱᴿ Semestre 2025-2026/TareaSemanal/Tarea2-4186.R
@@ -0,0 +1,59 @@
+###########################
+# SOCI 4186 -- sección 2   #
+# Nombre:                  #
+# Apellidos:               #
+# Tarea 2: Inicios de R    #
+###########################
+
+#En esta tarea harán lo siguiente
+# 1. Guardar esta tarea, cambiando el nombre de Tarea2-4186.R
+# a Apellido1-Apellido2-Tarea2.R 
+# 2. Completar las próximas secciones, guardando la secuencia
+# de comandos que usaron para cumplir con lo pedido. A modo de ejemplo
+# dejo una parte ya hecha. Continúen desde ahí.
+
+# Parte 0: Cargar datos ---------------------------------------------------
+# activarán el paquete 'dslabs', y pedirán con función para datos preguardados
+# 'murders', que cubre la cantidad de asesinatos en EEUU por armas de fuego en
+# el año 2010.
+
+library(dslabs)
+data("murders")
+
+# Parte 1: Revisión y manipulación inicial de datos -----------------------
+# 1. Visualicen las primeras seis filas usando el comando adecuado.
+
+#entre su contestación aquí entre la parte 1.1 y la 1.2, e igual en lo sucesivo
+
+# 2. Creen una variable adicional a las existentes, la tasa de asesinatos
+# esta debe ser por 100,000 habitantes. El nombre de la variable les toca 
+# a ustedes decidir
+
+
+# Parte 2: Creación de un objeto ------------------------------------------
+# 1. Crearán un objeto donde se almacene el nombre del estado con MAYOR
+# tasa de asesinatos por 100,000 habitantes.
+
+
+
+# 2. Crearán otro objeto donde se almacene el nombre del estado con MENOR
+# tasa de asesinatos por 100,000 habitantes.
+
+
+  
+# 3. ¿Qué estados son estos? En comentario indiquen el nombre de los estados
+# con mayor y menor tasa de asesinatos por 100,000 habitantes.
+
+#Ponga en formato de comentario las dos respuestas.
+
+# Parte 3: Creación de una imagen -----------------------------------------
+# 1. Crearán una imagen para representar los asesinatos de estas jurisdicciones
+# esta imagen podrá ser un diagrama de caja, un histograma, o un diagrama de
+# dispersión.
+
+# 2. Guardarán esta imagen como 'Imagen Tarea 2'. La enviarán, 
+# junto a la secuencia de comandos o 'script' modificado a Teams
+# con todas las funciones escritas, tal que yo pueda reproducir
+# lo que hicieron de principio a fin en mi ordenador personal.
+
+
